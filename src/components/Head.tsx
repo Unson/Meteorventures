c53bdabe460805899402dd3adf088e1ca73f201e
Wix Remote Machine <support@wix.com>
2026-03-06 18:00:52 +0000

    Update meta description

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,6 +3,7 @@ export const Head = () => {
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Helping businesses grow through strategic consulting, smart solutions, and practical guidance for modern challenges." />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
