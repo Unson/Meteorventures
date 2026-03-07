16ce050fcd17de975c278c13ffee8992e9c75b4a
Wix Remote Machine <support@wix.com>
2026-03-07 20:28:08 +0000

    Restored to checkpoint 6a560a061e21537760131cd9e545119776ab9f78

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,14 +2,13 @@ import { MemberProvider } from '@/integrations';
 import { createBrowserRouter, RouterProvider, Navigate, Outlet } from 'react-router-dom';
 import { ScrollToTop } from '@/lib/scroll-to-top';
 import ErrorPage from '@/integrations/errorHandlers/ErrorPage';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ServicesPage from './pages/ServicesPage';
-import InsightsPage from './pages/InsightsPage';
-import InsightDetailPage from './pages/InsightDetailPage';
-import FreeAuditPage from './pages/FreeAuditPage';
-import ContactPage from './pages/ContactPage';
-import PartnersPage from './pages/PartnersPage';
+import HomePage from '@/components/pages/HomePage';
+import AboutPage from '@/components/pages/AboutPage';
+import ServicesPage from '@/components/pages/ServicesPage';
+import InsightsPage from '@/components/pages/InsightsPage';
+import InsightDetailPage from '@/components/pages/InsightDetailPage';
+import FreeAuditPage from '@/components/pages/FreeAuditPage';
+import ContactPage from '@/components/pages/ContactPage';
 
 // Layout component that includes ScrollToTop
 function Layout() {
@@ -74,13 +73,6 @@ const router = createBrowserRouter([
         element: <ContactPage />,
         routeMetadata: {
           pageIdentifier: 'contact',
-        },
-      },
-      {
-        path: "partners",
-        element: <PartnersPage />,
-        routeMetadata: {
-          pageIdentifier: 'partners',
         },
       },
       {
