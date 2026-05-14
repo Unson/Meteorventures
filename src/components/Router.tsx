08fb626b27780a6a6cc40582d0ad907f86ff13a2
Wix Remote Machine <support@wix.com>
2026-05-14 03:41:18 +0000

    Fix page preview issue

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,9 +1,43 @@
 import { MemberProvider } from '@/integrations';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
-export default function AppRouter({ children }: { children: React.ReactNode }) {
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ServicesPage from './pages/ServicesPage';
+import InsightsPage from './pages/InsightsPage';
+import InsightDetailPage from './pages/InsightDetailPage';
+import FreeAuditPage from './pages/FreeAuditPage';
+import ContactPage from './pages/ContactPage';
+import PartnersPage from './pages/PartnersPage';
+import TransmissionsPage from './pages/TransmissionsPage';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+export default function AppRouter() {
   return (
     <MemberProvider>
-      {children}
+      <Router basename={import.meta.env.BASE_URL}>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/insights" element={<InsightsPage />} />
+          <Route path="/insights/:id" element={<InsightDetailPage />} />
+          <Route path="/free-audit" element={<FreeAuditPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/transmissions" element={<TransmissionsPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Router>
     </MemberProvider>
   );
 }
